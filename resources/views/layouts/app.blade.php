b18b1f57ef4d91ea66da6ba02c69a72702baf6ed
andrewrdev <84985099+andrewrdev@users.noreply.github.com>
2026-01-22 17:13:10 -0300

    feat: add boostrap css support

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title', 'Meu Sistema')</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+    <div class="container mt-4">
+        @yield('content')
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
